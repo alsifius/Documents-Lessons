78e7b6aa5b9ce8d281a6d94349c98573bc947c9a
alsifius <alsifius@gmail.com>
2021-07-08 11:27:13 -0500

    update

--- list_dir.py
+++ list_dir.py
@@ -1,8 +1,11 @@
-import os
+import os, glob
 print(os.listdir('.'))
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-for subjecID in files:
-    g = subjecID[0:8]
-    print (f"edf-anonymize.exe {g}.edf DeID/{g}.edf Mae Jemison %s" % g)
+#files = [f for f in os.listdir('.') if os.path.isfile(f) and re.match("\*.edf", f)]
+files = [f for f in glob.glob('*.edf')]
+print(files)
+for subjectID in files:
+    g = subjectID[0:8]
+    print (f"edf-anonymize.exe \'{subjectID}\' \'DeID/{subjectID}\' Mae Jemison %s" % g)
 
 print("done")
+#os.system('ls -l')
